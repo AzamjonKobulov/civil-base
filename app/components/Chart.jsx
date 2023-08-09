5d792ea82cd391dbcba02e5f9a036f835b2f07bd
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2023-08-09 21:26:20 +0500

    add/some features to the chart

--- app/components/Chart.jsx
+++ app/components/Chart.jsx
@@ -2,17 +2,22 @@
 
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
-import 'chartjs-plugin-datalabels'; // Import the plugin
+import 'chartjs-plugin-datalabels';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function Chart() {
+  let total = 120;
+  let remaining = 72;
+  let utilized = 48;
+  let remainingPercentage = (remaining / total) * 100;
+  let utilizedPercentage = (utilized / total) * 100;
+
   const data = {
     labels: ['Remaining', 'Utilized'],
     datasets: [
       {
-        label: 'Poll',
-        data: [48, 72],
+        data: [remaining, utilized],
         backgroundColor: ['#165BAA', '#A155B9'],
         borderColor: ['#165BAA', '#A155B9'],
       },
@@ -20,14 +25,12 @@ export default function Chart() {
   };
 
   const options = {
-    responsive: true,
-    maintainAspectRatio: false,
     plugins: {
       legend: {
         display: false,
       },
       datalabels: {
-        formatter: data,
+        display: false,
       },
     },
   };
@@ -37,14 +40,26 @@ export default function Chart() {
       <div className="w-2/3">
         <Doughnut data={data} options={options} />
       </div>
-      <div className="text-xs text-brand-gray-200 space-y-2 mt-3">
+      <div className="text-xs text-brand-gray-200 space-y-2">
         <div className="flex items-center space-x-2">
           <div className="w-5 h-2 bg-[#165BAA]"></div>
-          <p>Remaining</p>
+          <p className="flex items-center space-x-2">
+            <span>Remaining</span>
+            <span>
+              {remaining}
+              {/* ({remainingPercentage}%) */}
+            </span>
+          </p>
         </div>
         <div className="flex items-center space-x-2">
           <div className="w-5 h-2 bg-[#A155B9]"></div>
-          <p>Utilized</p>
+          <p className="flex items-center space-x-2">
+            <span>Utilized</span>
+          </p>
+          <span>
+            {utilized}
+            {/* ({utilizedPercentage}%) */}
+          </span>
         </div>
       </div>
     </div>
