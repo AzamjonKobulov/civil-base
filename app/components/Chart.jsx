a732e6121735bc9bc8d39e888b60e6ff909b0ad7
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2023-08-09 22:15:41 +0500

    add/some features to the chart

--- app/components/Chart.jsx
+++ app/components/Chart.jsx
@@ -7,11 +7,8 @@ import 'chartjs-plugin-datalabels';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function Chart() {
-  let total = 120;
   let remaining = 72;
   let utilized = 48;
-  let remainingPercentage = (remaining / total) * 100;
-  let utilizedPercentage = (utilized / total) * 100;
 
   const data = {
     labels: ['Remaining', 'Utilized'],
@@ -29,16 +26,42 @@ export default function Chart() {
       legend: {
         display: false,
       },
-      datalabels: {
-        display: false,
-      },
     },
   };
+
+  const plugins = [
+    {
+      id: 'insideLabelsPlugin',
+      afterDatasetsDraw(chart) {
+        const {
+          ctx,
+          data: { datasets },
+          chartArea: { width, height },
+        } = chart;
+
+        const chartData = chart.getDatasetMeta(0).data;
+
+        chartData.forEach((datapoint, i) => {
+          const { x, y } = datapoint.getCenterPoint();
+          const value = datasets[1].data[i];
+          const sumOfValues = datasets[1].data.reduce((acc, val) => acc + val);
+          ctx.textBaseline = 'middle';
+
+          const textX = x - ctx.measureText(value).width / 2;
+
+          if (value / sumOfValues > 0.05) {
+            ctx.fillStyle = '#f4f6f7';
+            ctx.fillText(datasets[1].data[i], textX, y);
+          }
+        });
+      },
+    },
+  ];
 
   return (
     <div className="flex">
       <div className="w-2/3">
-        <Doughnut data={data} options={options} />
+        <Doughnut data={data} options={options} plugins={plugins} />
       </div>
       <div className="text-xs text-brand-gray-200 space-y-2">
         <div className="flex items-center space-x-2">
