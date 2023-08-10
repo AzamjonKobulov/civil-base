b121c52489734b528c2e9e1925984431e20ac00a
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2023-08-10 09:42:07 +0500

    fix/export bill poppup

--- app/components/ExportBill.jsx
+++ app/components/ExportBill.jsx
@@ -22,7 +22,7 @@ export default function ExportBill({ closeModal, isOpen }) {
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <div className="flex justify-center p-4 text-center mt-32">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
